1c2f77632a72b562513b8787b089c56130443a5f
Tareq Munawer Taj <taj4580@gmail.com>
2025-04-29 11:06:23 +0600

    Initial project structure done

---
+++ src/test/java/com/bs23/taskmanagement/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs23.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
